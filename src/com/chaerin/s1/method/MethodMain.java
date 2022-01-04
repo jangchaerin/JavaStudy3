c8351ba5eae7f977b079f74185881ae6efb1c86d
jangchaerin <cofls3928@naver.com>
2022-01-04 14:35:03 +0900

    make project

---
+++ src/com/chaerin/s1/method/MethodMain.java
@@ -0,0 +1,12 @@
+package com.chaerin.s1.method;
+
+public class MethodMain {
+
+	public static void main(String[] args) {
+	
+		System.out.println("mainmethod start");
+		System.out.println("mainmethod finish");
+
+	}
+
+}
