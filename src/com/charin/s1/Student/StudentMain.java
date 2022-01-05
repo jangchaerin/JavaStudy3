4e4efabf73c6b14403d57ae37b1b283c82554c65
jangchaerin <cofls3928@naver.com>
2022-01-05 09:37:02 +0900

    student myyyy

--- src/com/charin/s1/Student/StudentMain.java
+++ src/com/charin/s1/Student/StudentMain.java
@@ -3,20 +3,21 @@ package com.charin.s1.Student;
 public class StudentMain {
 	public static void main(String[] args) {
 		System.out.println("start");
-		StudentUtil su = new StudentUtil();		
-	
-		Student student =su.makeStudent();
-		
-		
-		
+		StudentUtil su = new StudentUtil();
+
+		Student student[] = su.makeStudent();
+
 		System.out.println("이름\t번호\t국어\t영어\t수학\t");
-		System.out.println("--------------------------------");
-		System.out.print(student.name+"\t");
-		System.out.print(student.number+"\t");
-		System.out.print(student.kor+"\t");
-		System.out.print(student.eng+"\t");
-		System.out.println(student.math);
-	
+		System.out.println("--------------------------------------");
+		for (int i = 0; i < student.length; i++) {
+			Student stu = student[i];
+
+			System.out.print(stu.name + "\t");
+			System.out.print(stu.number + "\t");
+			System.out.print(stu.kor + "\t");
+			System.out.print(stu.eng + "\t");
+			System.out.println(stu.math);
+		}
 		System.out.println("finisth");
 	}
 
--- src/com/charin/s1/Student/StudentUtil.java
+++ src/com/charin/s1/Student/StudentUtil.java
@@ -3,22 +3,28 @@ package com.charin.s1.Student;
 import java.util.Scanner;
 
 public class StudentUtil {
-	public Student makeStudent(){
+	public Student [] makeStudent(){
 		Scanner sc = new Scanner(System.in);
+		System.out.println("학생 수를 입력하세요.");
+		int count = sc.nextInt();
+		//Student student = new Student();
+		Student student[] = new Student[count];
 		
-		Student student = new Student();
-		
+		for(int i=0;i<student.length;i++) {
+		Student stu = new Student();
 		System.out.println("이름을 입력하세요.");
-		student.name = sc.next();
+		stu.name = sc.next();
 		System.out.println("번호를 입력하세요.");
-		student.number = sc.nextInt();
+		stu.number = sc.nextInt();
 		System.out.println("국어 성적을 입력하세요.");
-		student.kor = sc.nextInt();
+		stu.kor = sc.nextInt();
 		System.out.println("영어 성적을 입력하세요.");
-		student.eng = sc.nextInt();
+		stu.eng = sc.nextInt();
 		System.out.println("수학 성적 입력하세요.");
-		student.math = sc.nextInt();
+		stu.math = sc.nextInt();
+		student[i]=stu;
 		
+	}
 		return student;
 	}
 }
